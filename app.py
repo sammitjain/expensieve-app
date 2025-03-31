bc342a76c26315c39eef73c8e3104464768b822c
Sammit Jain <sammitj@mathworks.com>
2025-03-31 19:10:10 +0530

    Enabled multi-file selections for receipts

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 # Initialize OpenAI client
-useOpenAI = True
+useOpenAI = False
 if useOpenAI:
     client = openai.OpenAI(api_key=OPENAI_API_KEY)
 
